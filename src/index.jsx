14f00eb98b336e68e922f840c4f3ee92052c211a
Pierre S <pierre.sarazin18@gmail.com>
2022-06-21 15:23:01 +0200

    initial app

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+    BrowserRouter,
+  } from 'react-router-dom';
+
+
+const Index = () => {
+    return (
+      <p>Salut</p>
+        
+    // <BrowserRouter>
+    //     <App />
+    // </BrowserRouter>   
+);
+}
+  ReactDOM.render(<Index />, document.getElementById('root'));
