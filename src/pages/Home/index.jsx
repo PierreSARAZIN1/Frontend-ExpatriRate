135c9c574efc8d4d6541b92cdc7859ff0b7fef7d
Bastien Arlot <arlot.b@gmail.com>
2022-06-27 11:11:37 +0200

    add logic to order card by country and add a search bar with cities filtered dynamicaly by name

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,12 +5,15 @@ import {API_URL} from "../../stores/api_url";
 import style from './style.css';
 
 
-
 const Home = () => {
 
     const [citiesList, setCitiesList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [page, setPage] = useState(6);
+    const [cityResult, setCityResult] = useState([]);
+    const [modal, setModal] = useState(false);
+    const [countries, setCountries] = useState([]);
+    const [countryFiltered, setCountryFiltered] = useState("");
 
     useEffect(
         () => {
@@ -23,35 +26,108 @@ const Home = () => {
                 .then((response) => response.json())
                 .then((response) => {
                     setCitiesList(response)
+                    setCityResult(response);
                     setIsLoading(false);
                 })
 
         }, []
     )
 
-  return (
-    <main>
-        <HeroBanner/>
-        {isLoading ?
-        null
-        :
-        <>
-            <div className="gridCards">
-                {citiesList.slice(0,page).map(city => <Card city={city} key={city.city.id}/>)}
-            </div>
+    useEffect(
+        () => {
+            fetch(API_URL + '/countries', {
+                method: 'get',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+                .then((response) => response.json())
+                .then((response) => {
+                    console.log(response)
+                    setCountries(response)
+                })
+        }, []
+    )
 
-            {citiesList.length > page? 
-                <div className='wrapper'>
-                    <button className='btn btn-primary showmore' onClick={()=> setPage(page + 6)}>Show more</button> 
-                </div>
-                
-            : null}
-        </>
-            
+    const filter = (e) => {
+        setModal(false);
+        if (e.target.value == undefined) {
+            setCityResult(citiesList);
+        } else {
+            if (countryFiltered !== "") {
+                setCityResult(citiesList.filter(country => country.country.name.toLowerCase() === countryFiltered.toLowerCase())
+                    .filter(
+                        city => city.city.name.toLowerCase().includes(e.target.value.toLowerCase())
+                    ))
+            } else {
+
+
+                setCityResult(citiesList.filter(
+                    city => city.city.name.toLowerCase().includes(e.target.value.toLowerCase())
+                ))
+            }
         }
+    }
 
-    </main>
-  );
+    const openModal = (e) => {
+        e.preventDefault();
+        setModal(!modal);
+    }
+    const filterByCountry = (e) => {
+        setCountryFiltered(e.target.textContent);
+        setCityResult(citiesList.filter(
+            city => city.country.name === e.target.textContent
+        ));
+        setModal(false);
+    }
+
+    return (
+        <main>
+            <HeroBanner/>
+            <>
+                <div className="searchBar">
+                    <button type="submit" className="searchByCountry" onClick={openModal}>Filter by Country
+                    </button>
+                    <div className="searchBarContent">
+                        <div className='filter'>
+                            <input type="text" placeholder="Search City" onChange={filter}></input>
+                        </div>
+                        {modal ?
+                            <div className="modal">
+                                {countries.map(country => {
+                                    return (<p className="countryFiltered"
+                                               onClick={filterByCountry}>{country.country.name}</p>)
+                                })}
+                            </div>
+                            :
+                            null
+                        }
+                        <p onClick={() => setCountryFiltered("")}>{countryFiltered}</p>
+                    </div>
+
+                </div>
+            </>
+            {isLoading ?
+                null
+                :
+                <>
+                    <div className="gridCards">
+                        {cityResult.slice(0, page).map(city => <Card city={city} key={city.city.id}/>)}
+                    </div>
+
+                    {cityResult.length > page ?
+                        <div className='wrapper'>
+                            <button className='btn btn-primary showmore' onClick={() => setPage(page + 6)}>Show more
+                            </button>
+                        </div>
+
+                        : null}
+                </>
+
+            }
+
+        </main>
+    );
 };
 
 export default Home;
