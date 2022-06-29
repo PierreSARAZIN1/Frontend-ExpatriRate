e3545b06afd4d9a117671a6bdbecc4a192162fbf
Bastien Arlot <arlot.b@gmail.com>
2022-06-29 15:45:15 +0200

    fix style on privacy policy, fix link on footer for privacy policy, add situational show of cookies when logged

--- src/components/Cookie/index.jsx
+++ src/components/Cookie/index.jsx
@@ -1,14 +1,20 @@
-import React, { useRef } from "react";
+import React, {useEffect, useRef} from "react";
 import './style.css';
 import Cookies from "js-cookie";
 import { cookieAtom } from "stores/user";
 import { useSetAtom } from "jotai";
 import { Link } from 'react-router-dom';
+import {jwtAtom} from "stores/user";
+import {useAtomValue} from "jotai";
+
 
 const Cookie = () => {
 
   const cookiepopup = useRef(null);
   const setCookieChoice = useSetAtom(cookieAtom);
+  const jwt = useAtomValue(jwtAtom);
+
+
 
 
   const acceptcookie = () =>{
@@ -25,16 +31,20 @@ const Cookie = () => {
 
 
   return(
-    <div className="cookie" ref={cookiepopup}>
-      <div className="cookiecontent">
-        <p> This site uses cookies that allow us to provide you with an optimal browsing experience, such as maintaining your connection when you close the web page. Please refer to your <Link to="/privacy_policy">Privacy Policy</Link> for more information.</p>
-        <div className="cookiechoice">
-          <button className="btn btn-primary" onClick={() => acceptcookie()}>Allow</button>
-          <button className="btn btn-primary" onClick={() => refusecookie()}>Decline</button>
-        </div>
+      jwt !==  "" ?
+          null
+          :
+  <div className="cookie" ref={cookiepopup}>
+    <div className="cookiecontent">
+      <p> This site uses cookies that allow us to provide you with an optimal browsing experience, such as maintaining
+        your connection when you close the web page. Please refer to our <Link to="/privacy_policy">Privacy
+          Policy</Link> for more information.</p>
+      <div className="cookiechoice">
+        <button className="btn btn-primary" onClick={() => acceptcookie()}>Allow</button>
+        <button className="btn btn-primary" onClick={() => refusecookie()}>Decline</button>
       </div>
     </div>
-  )
+  </div>)
 }
 
 export default Cookie;
